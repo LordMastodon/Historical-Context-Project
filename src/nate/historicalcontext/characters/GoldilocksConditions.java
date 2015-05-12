abec14de3676493892f59849702ca6c38838f32b
LordMastodon <gsapiran@gmail.com>
2015-05-12 19:19:56 -0400

    Added Goldilocks Conditions Stuff

--- src/nate/historicalcontext/characters/GoldilocksConditions.java
+++ src/nate/historicalcontext/characters/GoldilocksConditions.java
@@ -17,8 +17,15 @@ public class GoldilocksConditions extends Story {
 
 			@Override
 			public void content() throws InterruptedException {
-				
-				
+				Thread.sleep(3000);
+				printToConsole("This was a very good time for Java to proliferate. That's because");
+				Thread.sleep(3000);
+				printToConsole("at that tme there was C++, but people needed a programming language that could do");
+				Thread.sleep(3000);
+				printToConsole("what C++ couldn't. Java came at the perfect time while people were easing into the");
+				Thread.sleep(3000);
+				printToConsole("Internet. ");
+
 				setStoryFinished(true);
 			}
 			
